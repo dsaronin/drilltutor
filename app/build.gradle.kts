54c3822d1dc1b6267cebeafa5d353b195678777f
dsaronin <dsaronin@gmail.com>
2026-01-20 17:54:00 -0800

    versioning added to module build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,9 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -14,9 +20,16 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "v0.0.1"
+
+        buildConfigField("String", "DRILLTUTOR_VERSION_CODE", "\"1\"")
+        buildConfigField("String", "DRILLTUTOR_MY_VERSION", "\"V0.1\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val buildDateStamp = SimpleDateFormat("yyyyMMdd", Locale.US).format(Date())
+        buildConfigField("String", "BUILD_TIME", "\"$buildDateStamp\"")
+        resValue("string", "build_time", buildDateStamp)
     }
 
     buildTypes {
@@ -29,11 +42,12 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
@@ -53,4 +67,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_17)
+    }
 }
